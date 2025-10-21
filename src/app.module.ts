b9263157937dd9c43b348bbf5f05324e9b3b507a
lucashrv <lucas.hrv@outlook.com>
2025-10-21 06:58:58 -0300

    chore: .env and class-validator / transformer

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from "@nestjs/common";
 import { AuthModule } from "./modules/auth/auth.module";
 import { UserModule } from "./modules/user/user.module";
 import { DatabaseModule } from "./database/database.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-    imports: [DatabaseModule, UserModule, AuthModule],
+    imports: [
+        DatabaseModule,
+        UserModule,
+        AuthModule,
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+    ],
 })
 export class AppModule {}
