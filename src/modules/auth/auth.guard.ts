3eb31846cf2bdb8eca9e2996f125844354748d64
lucashrv <lucas.hrv@outlook.com>
2025-12-02 11:41:12 -0300

    feat: create AuthGuard

---
+++ src/modules/auth/auth.guard.ts
@@ -0,0 +1,36 @@
+import {
+    CanActivate,
+    ExecutionContext,
+    Injectable,
+    UnauthorizedException,
+} from "@nestjs/common";
+import { Request } from "express";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const req: Request & { user?: any } = context
+            .switchToHttp()
+            .getRequest();
+        const token = req.cookies?.session as string;
+
+        if (!token) throw new UnauthorizedException("Não autenticado");
+
+        const session = await this.prisma.session.findUnique({
+            where: { sessionToken: token },
+            include: { user: true },
+        });
+
+        if (!session) throw new UnauthorizedException("Seção inválida");
+        if (session.revoked) throw new UnauthorizedException("Seção revogada");
+        if (session.expires < new Date())
+            throw new UnauthorizedException("Seção expirada");
+
+        req.user = session.user;
+
+        return true;
+    }
+}
